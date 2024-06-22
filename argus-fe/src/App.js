dbddde66240217194e4c10bcab349b7dd31adeec
vaarvik <47792591+vaarvik@users.noreply.github.com>
2024-06-22 19:38:37 +0200

    Add header web component to angular application

--- argus-fe/src/App.js
+++ argus-fe/src/App.js
@@ -5,11 +5,11 @@ import 'shared-web-components';
 function App() {
     return (
         <div className="App">
+            <header-component text="Hola!"></header-component>
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>ARGUS BRO!</p>
             </header>
-            <header-component text="Hola!"></header-component>
         </div>
     );
 }
